ba6c474dfa40d272c51c626da5df851185c8eb02
meredith-jones <meredithjw@gmail.com>
2016-12-09 14:44:17 -0800

    Fix merge conflict.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209071333) do
 ActiveRecord::Schema.define(version: 20161209184912) do
 
   # These are extensions that must be enabled in order to support this database
@@ -39,7 +38,6 @@ ActiveRecord::Schema.define(version: 20161209184912) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
