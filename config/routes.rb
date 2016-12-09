e70403a6dff4026da0cd87e09ed0a2742ce2fe26
Long Tran <artsandcrass@gmail.com>
2016-12-09 12:15:18 -0800

    changed chat routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
+
+
+  resources :chatrooms, param: :slug
+  resources :messages
 end
--- db/migrate/20161209071333_create_chatrooms.rb
+++ db/migrate/20161209071333_create_chatrooms.rb
@@ -1,6 +1,9 @@
 class CreateChatrooms < ActiveRecord::Migration[5.0]
   def change
     create_table :chatrooms do |t|
+      t.integer :user_id
+      t.integer :topic_id
+      t.string  :slug
 
       t.timestamps
     end
